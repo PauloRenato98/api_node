c22cfa7f4a30f7fa2abca7d74e19bb0cfed402bf
PauloRenato98 <paulo__159@hotmail.com>
2020-01-12 11:42:20 -0300

    adicionando Cadastro das Igrejas

--- src/model/igreja.js
+++ src/model/igreja.js
@@ -16,30 +16,10 @@ const IgrejaSchema = new Schema({
         type: String,
         required: true,
     },
-    /*usuario: {
-        type: String,
-        required: true,
-    },*/
-    password: {
-        type: String,
-        required: true,
-        select: false,
-    },
-    /* email: {
-         type: String,
-         required: true,
-     },*/
+
 },
     { timestamps: true, }
 );
-
-/*IgrejaSchema.pre('save', async function (next) {
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-
-    next();
-})*/
-
 
 module.exports = model('Igreja', IgrejaSchema);
 
